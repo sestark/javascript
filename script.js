aa04020fd916b4f7b9c81b807a5224023412aa3c
Loyola User <confer15@luc.edu>
2018-10-27 11:39:13 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	
+    $("h2").click(function(){
+        $("p").slideToggle();
+		$("body").css("background-color", "red");
+		
+    });
+	
+<p id="demo"></p>
+
+});
